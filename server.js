29ea8a44b0bee8dbd1b6519126080951670e2657
Avishek <choudhury.avishek1@tcs.com>
2020-08-05 00:19:37 +0530

    server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const htmlPath = path.join(__dirname, 'dist/hacker-news-clone');
+const htmlPath = path.join(__dirname, 'dist/hacker-news-clone/browser');
 
 app.use(express.static(htmlPath));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname+'/dist/hacker-news-clone/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/hacker-news-clone/browser/index.html'));
     });
 
 const server = app.listen(process.env.PORT || 3000, function () {
