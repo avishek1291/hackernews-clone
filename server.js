c76d4c4b8c2bf18ce74960d0ad1994f68d164676
Avishek <choudhury.avishek1@tcs.com>
2020-08-04 23:43:51 +0530

    script change

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const htmlPath = path.join(__dirname, 'dist/hacker-news-clone');
 
 app.use(express.static(htmlPath));
 
+app.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname+'/dist/hacker-news-clone/index.html'));
+    });
+
 const server = app.listen(process.env.PORT || 3000, function () {
     const host = 'localhost';
     const port = server.address().port;
